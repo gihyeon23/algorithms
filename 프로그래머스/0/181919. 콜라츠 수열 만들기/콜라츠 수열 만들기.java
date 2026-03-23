cf1d159132abf2442d5aeeb3b26c5853ed707628
gihyeon23 <gihyeon9412@inu.ac.kr>
2026-03-23 19:51:12 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.06 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        list.add(n);
+        
+        while(n != 1) {
+            if(n % 2 == 0) {
+                n /= 2;
+                list.add(n);
+            } else {
+                n = 3 * n + 1;
+                list.add(n);
+            }
+        }
+        return list;
+    }
+}
