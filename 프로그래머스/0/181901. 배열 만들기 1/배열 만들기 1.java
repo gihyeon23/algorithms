51af2d9cda88adbfb31cd085f1f507d299e0783e
gihyeon23 <gihyeon9412@inu.ac.kr>
2026-03-31 21:23:53 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.10 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int k) {
+        int count = 0;
+        int tmp = k;
+        for(int i = k; i <= n; i += k) {
+            count++;
+        }
+        int[] answer = new int[count];
+        
+        for(int i = 0; i < count; i++) {
+            answer[i] = k;
+            k = k + tmp;
+        }
+        
+        return answer;
+    }
+}
