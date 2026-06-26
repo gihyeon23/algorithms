7908adfe8fedc7849bc05df837d4e457d3d4f7b8
gihyeon23 <gihyeon9412@inu.ac.kr>
2026-06-27 00:42:59 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 33.96 ms, Memory: 108 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int solution(int[] arr) {
+        int answer = 0;
+
+        while(true) {
+            int before = 0;
+            boolean b = false; //바뀐거 없음
+            
+            for(int i = 0; i < arr.length; i++) {
+                before = arr[i];
+                if(arr[i] >= 50 && arr[i] % 2 == 0) {
+                    arr[i] = arr[i] / 2;
+                    
+                    } else if (arr[i] <= 50 && arr[i] % 2 == 1) {
+                        arr[i] = arr[i] * 2 + 1;
+                    }
+                
+                    if(before != arr[i]) {
+                            b = true;
+                    }   
+                }
+            
+            if(b == false) {
+                break;
+            }
+            
+            answer++;
+        }
+        return answer;
+    }
+}
