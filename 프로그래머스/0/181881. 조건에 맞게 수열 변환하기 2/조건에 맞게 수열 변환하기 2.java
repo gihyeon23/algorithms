a3819abc8b29d6f3ae0647bde7d9b19189d0894d
gihyeon23 <gihyeon9412@inu.ac.kr>
2026-06-27 00:45:08 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 32.37 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -14,12 +14,12 @@ class Solution {
                     } else if (arr[i] <= 50 && arr[i] % 2 == 1) {
                         arr[i] = arr[i] * 2 + 1;
                     }
-                
+                    //하나라도 바뀌면 스위치 바꾸기
                     if(before != arr[i]) {
                             b = true;
                     }   
                 }
-            
+            //스위치 바꾸기를 하나도 안한다면 변한게 없다는 뜻 탈출
             if(b == false) {
                 break;
             }
