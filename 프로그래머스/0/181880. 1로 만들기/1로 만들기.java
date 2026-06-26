127aab862c6b05fe142b0998c97d06040a009a99
gihyeon23 <gihyeon9412@inu.ac.kr>
2026-06-27 01:04:09 +0900

    [level 0] Title: 1로 만들기, Time: 3.41 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int count = 0;
+        
+        for (int i = 0; i < num_list.length; i++) {
+            while (num_list[i] != 1) {
+                if(num_list[i] % 2 == 0) {
+                    num_list[i] = num_list[i] / 2;
+                    count++;
+                    System.out.print(num_list[i] + " (1) ");
+                } else {
+                    num_list[i] = (num_list[i] - 1) / 2;
+                    count++;
+                    System.out.print(num_list[i] + " (2) ");
+                }
+            }
+            System.out.println();
+        }
+        
+        return count;
+    }
+}
