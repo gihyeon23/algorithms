3b8848810c0e712b04cd470fa523e5954325b672
gihyeon23 <gihyeon9412@inu.ac.kr>
2026-06-29 23:06:49 +0900

    [level 0] Title: 세 개의 구분자, Time: 24.65 ms, Memory: 99.6 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,35 +1,8 @@
 class Solution {
     public String[] solution(String myStr) {
         String[] answer = {};
-        String[] answerLast = {};
-        int count = 0;
-        int index = 0;
+        myStr = myStr.replaceAll("[abc]+", " ").trim();
 
-        myStr = myStr.replace("a", " ").replace("b", " ").replace("c", " ");
-        
-        myStr = myStr.trim();
-        answer = myStr.split(" ");
-        
-        if(answer[0].equals("")) {
-            answer[0] = "EMPTY";
-        }
-        
-        for(int i = 0; i < answer.length; i++) {
-            if(!answer[i].equals("")) {
-                count++;
-            }
-        }
-        System.out.print(count);
-        
-        answerLast = new String[count];
-        for(int i = 0; i < answer.length; i++) {
-            if(!answer[i].equals("")) {
-                answerLast[index] = answer[i];
-                index++;
-            }
-        }
-        
-        
-        return answerLast;
+        return myStr.isEmpty() ? new String[]{"EMPTY"} : myStr.split(" ");
     }
 }
