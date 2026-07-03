4772f09a7557fea9fd5a11a9e6c064a449e69222
gihyeon23 <gihyeon9412@inu.ac.kr>
2026-07-03 22:09:50 +0900

    [level 0] Title: 문자열 묶기, Time: 7.04 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -2,40 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] strArr) {
-        int[] count = new int[strArr.length];
+        
+        int[] strLength = new int[31]; //만약 30을 담고 ++해버리면 또 배열 터지니까 걍 하나더
         int answer = 0;
         
-        // 문자열 길이 저장
-        for (int i = 0; i < strArr.length; i++) {
-            count[i] = strArr[i].length();
+        for(int i = 0; i < strArr.length; i++) {
+            strLength[strArr[i].length()]++;
         }
         
-        // 길이 정렬
-        Arrays.sort(count);
+        Arrays.sort(strLength);
         
-        int bN = count[0];
-        int nowCount = 1;
-        
-        for (int i = 1; i < strArr.length; i++) {
-            if (bN == count[i]) {
-                nowCount++;
-            } else {
-                // 지금까지 같은 길이였던 그룹 크기 저장
-                if (answer < nowCount) {
-                    answer = nowCount;
-                }
-                
-                // 새 그룹 시작
-                bN = count[i];
-                nowCount = 1;
-            }
-        }
-        
-        // 마지막 그룹 처리
-        if (answer < nowCount) {
-            answer = nowCount;
-        }
-        
-        return answer;
+        return strLength[30];
     }
 }
