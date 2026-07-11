dadcccffd630ee2efd4f1281b4c093f166b69f11
gihyeon23 <gihyeon9412@inu.ac.kr>
2026-07-11 22:29:58 +0900

    [level 2] Title: 전화번호 목록, Time: 222.79 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,9 +3,10 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        String tmp = phone_book[0];
 
         Arrays.sort(phone_book);
+        
+        String tmp = phone_book[0];
         
         for(int i = 1; i < phone_book.length; i++) {
             if(phone_book[i].startsWith(tmp)) {
