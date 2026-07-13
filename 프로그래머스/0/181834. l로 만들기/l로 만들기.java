7d549501adcf5594d3ab1017ca982c0060f8806a
gihyeon23 <gihyeon9412@inu.ac.kr>
2026-07-13 11:43:45 +0900

    [level 0] Title: l로 만들기, Time: 9.11 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,16 +1,6 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        
-        
-        for(int i = 0; i < myString.length(); i++) {
-            if(myString.charAt(i) < 'l') {
-                answer += "l";
-            } else {
-                answer += myString.charAt(i);
-            }
-        }
-        
-        return answer;
+
+        return myString.replaceAll("[^m-z]", "l");
     }
 }
