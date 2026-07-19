baa52830b6f25f7a1095aa91a5af45027a912a90
gihyeon23 <gihyeon9412@inu.ac.kr>
2026-07-19 23:22:50 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,19 +1,5 @@
 class Solution {
     public int solution(int n, int k) {
-        int answer = 0;
-        
-        for(int i = 1; i <= n; i++) {
-            answer += 12000;
-                if(i % 10 == 0) {
-                    System.out.println(i);
-                    answer -= 2000;
-            }
-        }
-        
-        for(int i = 0; i < k; i++) {
-            answer += 2000;
-        }
-        
-        return answer;
+        return n * 12000 + k * 2000 - n/10 * 2000;
     }
 }
