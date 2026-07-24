daa0ca2fdd799a00e8d9e1faec4d5d86c8a61c42
gihyeon23 <gihyeon9412@inu.ac.kr>
2026-07-24 12:15:33 +0900

    [level 0] Title: 주사위의 개수, Time: 0.02 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -1,12 +1,5 @@
 class Solution {
     public int solution(int[] box, int n) {
-        int answer = 0;
-        int bu = 1;
-        
-        for(int i = 0; i < box.length; i++) {
-            bu *= box[i] / n;
-        }
-        
-        return bu;
+        return (box[0] / n) * (box[1] / n) * (box[2] / n);
     }
 }
