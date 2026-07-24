07f419c44d9f943201151408b0f5ff2e0496b893
gihyeon23 <gihyeon9412@inu.ac.kr>
2026-07-24 12:52:52 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -1,22 +1,13 @@
 class Solution {
     public int solution(int n) {
-        int answer = 1;
-        int fa = 1;
-        int c = 1;
-        
-        while(true) {
-            int tmp = 1;
-            for(int i = c; i > 0; i--) {
-                tmp *= i;
-            }
-            
-            fa = tmp;
-            c++;
-            if(fa > n) {
-                break;
-            }
+        int factorial = 1;
+        int i = 1;
+
+        while (factorial * (i + 1) <= n) {
+            i++;
+            factorial *= i;
         }
-        
-        return c - 2;
+
+        return i;
     }
 }
