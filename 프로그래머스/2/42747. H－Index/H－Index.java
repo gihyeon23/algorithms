47e7f52cc056aef4859407bb7b5668c02c8f7ff0
gihyeon23 <gihyeon9412@inu.ac.kr>
2026-07-27 12:47:32 +0900

    [level 2] Title: H-Index, Time: 3.25 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,18 +2,10 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] citations) {
         Arrays.sort(citations);
-        int answer = 0;
-
-        for (int i = 0; i <= citations.length; i++) {
-            // h 이상 인용된 논문 수 세기
-            int count = 0;
-            for (int c : citations) {
-                if (c >= i) count++;
-            }
-            // 조건 만족하면 이 h를 후보로 기록
-            if (count >= i) answer = i;
-            System.out.println(i + " " + count);
+        int n = citations.length;
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n - i) return n - i;
         }
-        return answer;
+        return 0;
     }
 }
